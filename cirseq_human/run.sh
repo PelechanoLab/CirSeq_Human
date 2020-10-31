fe7517fb3f6a0597f80df34cb6ecbfe24d240bc5
Jingwen Wang <jingwen.wang@scilifelab.se>
2020-10-31 23:41:43 +0100

    Change aligner to HISAT2 for allowing splicing detection

--- cirseq_human/run.sh
+++ cirseq_human/run.sh
@@ -10,37 +10,32 @@ set -e
 
 WORKDIR=$1
 REFFILE=$2
-SCRIPTDIR=$3
-FASTQS="${@:4}"
+THREAD=$3
+SCRIPTDIR=$4
+SPLICESITE=$5
+FASTQS="${@:6}"
 
-#clean up
-mkdir -p ${WORKDIR}/index
-
-# clean up reference file path, just get the filename
-REFNAME=`echo $REFFILE | sed -e 's/.*\///g' | awk -F '.' '{print $1}'`
-REFIDX=${WORKDIR}/index/${REFNAME}
-
-# build bowtie index
-bowtie2-build ${REFFILE} ${REFIDX}
+#check if working directory exists
+mkdir -p $WORKDIR
 
 # generate consensus
 python ${SCRIPTDIR}/ConsensusGeneration.py $WORKDIR $FASTQS
 
 # align consensus
-bowtie2 -q --phred33 --no-hd --no-sq --local -x ${REFIDX} -U ${WORKDIR}/1_consensus.fastq.gz | gzip -c > ${WORKDIR}/2_alignment.sam.gz
+sat2 -p $THREAD --no-hd --no-sq -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/1_consensus.fastq.gz | gzip -c > ${WORKDIR}/2_alignment.sam.gz
 
 # preprocess 1
 python ${SCRIPTDIR}/preprocessing_1.py ${WORKDIR}
 
 # align again
-bowtie2 -q --phred33 --no-unal --no-hd --no-sq --local -x ${REFIDX} -U ${WORKDIR}/4_rearranged.fastq.gz | gzip -c > ${WORKDIR}/6_alignment.sam.gz
+hisat2 -p $THREAD --no-unal --no-hd --no-sq -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/4_rearranged.fastq.gz | gzip -c > ${WORKDIR}/6_alignment.sam.gz
 
 # preprocess 2
 python ${SCRIPTDIR}/preprocessing_2.py ${WORKDIR} 
 
-bowtie2 -q --phred33 --no-unal --no-hd --no-sq --local -x ${REFIDX} -U ${WORKDIR}/5_rotated.fastq.gz | gzip -c > ${WORKDIR}/9_alignment.sam.gz
+hisat2 -p $THREAD --no-unal --no-hd --no-sq -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/5_rotated.fastq.gz | gzip -c > ${WORKDIR}/9_alignment.sam.gz
 
-bowtie2 -q --phred33 --no-unal --no-hd --no-sq --local -x ${REFIDX} -U ${WORKDIR}/8_rotated.fastq.gz | gzip -c > ${WORKDIR}/10_alignment.sam.gz
+hisat2 -p $THREAD --no-unal --no-hd --no-sq -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/8_rotated.fastq.gz | gzip -c > ${WORKDIR}/10_alignment.sam.gz
 
 # preprocess 3
 python ${SCRIPTDIR}/preprocessing_3.py ${WORKDIR} 
@@ -48,6 +43,3 @@ python ${SCRIPTDIR}/preprocessing_3.py ${WORKDIR}
 cat ${WORKDIR}/3_alignment.sam.gz ${WORKDIR}/7_alignment.sam.gz ${WORKDIR}/11_alignment.sam.gz > ${WORKDIR}/data.sam.gz
 rm -f ${WORKDIR}/3_alignment.sam.gz ${WORKDIR}/7_alignment.sam.gz ${WORKDIR}/11_alignment.sam.gz ${WORKDIR}/2_alignment.sam.gz ${WORKDIR}/4_rearranged.fastq.gz ${WORKDIR}/5_rotated.fastq.gz ${WORKDIR}/6_alignment.sam.gz ${WORKDIR}/8_rotated.fastq.gz ${WORKDIR}/9_alignment.sam.gz ${WORKDIR}/10_alignment.sam.gz
 
-python ${SCRIPTDIR}/QualityFilter.py ${WORKDIR} ${REFFILE}
-
-Rscript ${SCRIPTDIR}/ParameterPlots.R ${WORKDIR}
