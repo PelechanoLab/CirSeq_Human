140d53c5f6a06727dce428883bc5a3e47813e39f
Jingwen Wang <jingwen.wang@scilifelab.se>
2020-11-01 22:55:20 +0100

    Update tasks in the pipeline

--- cirseq_human/run.sh
+++ cirseq_human/run.sh
@@ -28,23 +28,29 @@ fi
 python ${SCRIPTDIR}/ConsensusGeneration.py $WORKDIR $FASTQS
 
 # align consensus
+echo "Aligning all consensus sequences..."
 hisat2 -p $THREAD -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/1_consensus.fastq.gz | samtools view -@ $THREAD -b -o ${WORKDIR}/2_alignment.bam
 
 # preprocess 1
+echo "Extracting perfectly mapped reads and rearranging/rotating the rest..."
 python ${SCRIPTDIR}/preprocessing_1.py ${WORKDIR}
 
 # align again
+echo "Aligning the rearranged sequences..."
 hisat2 -p $THREAD --no-unal -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/4_rearranged.fastq.gz | samtools view -@ $THREAD -b -o ${WORKDIR}/6_alignment.bam
 
 # preprocess 2
+echo "Extracting perfectly mapped reads and rotating the rest..."
 python ${SCRIPTDIR}/preprocessing_2.py ${WORKDIR} 
 
+echo "Aligning the rotated sequences..."
 hisat2 -p $THREAD --no-unal -x ${REFDIR_IDX} --known-splicesite-infile ${SPLICESITE} -U ${WORKDIR}/5_rotated.fastq.gz,${WORKDIR}/8_rotated.fastq.gz | samtools view -@ $THREAD -b -o ${WORKDIR}/9_alignment.bam
 
 # preprocess 3
+echo "Extracting the best hit of each perfectly mapped sequence..."
 python ${SCRIPTDIR}/preprocessing_3.py ${WORKDIR} 
 
-echo "start merging alignment file"
+echo "Combining alignment files and sorting"
 samtools merge -f -@ $THREAD ${WORKDIR}/consensus_alignment.bam ${WORKDIR}/3_alignment.bam ${WORKDIR}/7_alignment.bam ${WORKDIR}/10_alignment.bam
 samtools sort -@ $THREAD -o ${WORKDIR}/consensus_align_sorted.bam ${WORKDIR}/consensus_alignment.bam
 rm -f ${WORKDIR}/4_rearranged.fastq.gz ${WORKDIR}/*_rotated.fastq.gz ${WORKDIR}/*_alignment.bam
